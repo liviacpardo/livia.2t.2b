7cf4cd96d46001ef4abb59b03dc652698c73dcf3
liviacpardo <livia.pardo@escola.pr.gov.br>
2025-05-16 16:03:11 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,58 +1,58 @@
 criaCartao(
     'Ferrari',
-    'Quem são os pilots e chef de equipe atuais da equipe Ferrari?',
-    'Lewis Hamilton, Charles Leclerc e Frédéric Vasseur.'
+    'Quem são os pilotos atuais da equipe Ferrari?',
+    'Lewis Hamilton, Charles Leclerc.'
 )
 
 criaCartao(
-    'Airton Senna',
-    'O que seria o zero da função?',
-    'O valor onde o gráfico cruza o eixo da abscissa, ou ainda o valor de x para quando y for igual a 0.'
+    'Ayrton Senna',
+    'Quando morreu Ayton Senna?',
+    '1 de maio de 1994.'
 )
 
 criaCartao(
     'Circuítos',
-    'Qual a definição de média aritmética?',
-    'Resultado da divisão do somatório dos números dados pela quantidade de números somados.'
+    'Quantos circuítos possui a temporada de 2025?',
+    '24 circuítos, incluindo autódrmos e corridas de rua.'
 )
 
 criaCartao(
     'Mc Laren',
-    'Como se calcula a área de um triângulo qualquer?',
-    'Multiplique a base pela altura e divida por dois.'
+    'Quantos títulos a Mc Laren possui atualmente (maio 2025)?',
+    ' 9 títulos mundiais de construtores e 12 títulos mundiais de pilotos.'
 )
 
 criaCartao(
     'Lewis Hamilton',
-    'O que define o conjunto dos números racionais, dê exemplos?',
-    'Números que podem ser escritos na forma de fração. Por exemplo, 3/4, 10/2, -8/2'
+    'Qantos títulos pela F1 possui o piloto Lewis Hamilton?',
+    '7 títulos mundiais,  sendo eles: 2008, 2014, 2015, 2017, 2018, 2019 e 2020'
 )
 
 criaCartao(
     'Michael Schumacher',
-    'Quando utilizo o teorema de Pitágoras?',
-    'Para encontrar um dos lados de um triângulo retângulo, sendo dado os outros dois lados. h² = c² + c².'
+    'Qual foi a ultima equipe e o ultímo circuíto que Michael Schumacher correu plea ultíma vez antes de seu acidente?',
+    'Equipe Mercedes, no circuíto de Interlagos, no Brasil, em 2012.'
 )
 
 criaCartao(
     'Campeão',
-    'Qual o valor de 58 - 2 x 10?',
-    '38.'
+    'Quem foi o ultímo campeão mundial de Formúla 1 (2024)?',
+    'Max Verstappen.'
 )
 
 criaCartao(
     'Pilotos',
-    'Qual o valor de cosseno de 60º?',
-    '1/2.'
+    'Quantos pilotos disputam disputam atualmente (2025) o campeonato automobilístico de Fórmula 1?',
+    '20 pilotos.'
 )
 
 criaCartao(
     'Equipes',
-    'Qual a ordem das operações matemática, da com maior para a menor prioridade?',
-    'Parênteses, Expoentes, Multiplicação e Divisão, Adição e Subtração.'
+    'Quantas e quais são as equipes atuais (2025)?',
+    '10 equipes, Aston Martin, BWT Alpine, McLaren, Mercedes, Haas, Red Bull, Scuderia Ferrari, Stake Kick Sauber, Visa Cash App Racing Bulls, Williams.'
 )
 criaCartao(
-    'Formula 1',
-    'Qual a ordem das operações matemática, da com maior para a menor prioridade?',
-    'Parênteses, Expoentes, Multiplicação e Divisão, Adição e Subtração.'
+    'Fórmula 1',
+    'Quantos anos tem a Fórmula 1 e onde foi a primeira corrida?',
+    'Completa 75 anos neste ano de 2025. A primeira corrida oficial ocorreu em Silverstone, Inglaterra.'
 )
